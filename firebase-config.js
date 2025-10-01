22473b58173b407c3b574b987d0c15b82ab20577
ILMoretto <morete.alencar@gmail.com>
2025-10-01 10:53:18 -0400

    Corrige firebase-config.js com valores reais - resolve problema de conexão

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@
 // Este arquivo é um template. As variáveis serão substituídas pelo GitHub Actions.
 
 const firebaseConfig = {
-  apiKey: "${FIREBASE_API_KEY}",
-  authDomain: "${FIREBASE_AUTH_DOMAIN}",
-  databaseURL: "${FIREBASE_DATABASE_URL}",
-  projectId: "${FIREBASE_PROJECT_ID}",
-  storageBucket: "${FIREBASE_STORAGE_BUCKET}",
-  messagingSenderId: "${FIREBASE_MESSAGING_SENDER_ID}",
-  appId: "${FIREBASE_APP_ID}"
+  apiKey: "AIzaSyCyLXPt2eh8nLmAG34wmEcrt-SkkWFgl3Q",
+  authDomain: "chatpad-app-2712e.firebaseapp.com",
+  databaseURL: "https://chatpad-app-2712e-default-rtdb.firebaseio.com",
+  projectId: "chatpad-app-2712e",
+  storageBucket: "chatpad-app-2712e.firebasestorage.app",
+  messagingSenderId: "991519277155",
+  appId: "1:991519277155:web:d91bfcfe2212d905227856"
 };
 
 // Inicializar Firebase
